dbee5e78b9f8f2396a27e365fc9da698d6c186b2
s2402361 <s2402361@usls.edu.ph>
2026-02-12 02:33:05 +0800

    Update config.js

--- config.js
+++ config.js
@@ -33,7 +33,7 @@ const CONFIG = {
             nextBtn: "Next ❤️"                                         // Text for the next button
         },
         third: {
-            text: "Will you be my Valentine on February 14th, 2025? 🌹", // The big question!
+            text: "Will you be my Valentine on February 14th, 2026? 🌹", // The big question!
             yesBtn: "Yes!",                                             // Text for "Yes" button
             noBtn: "No"                                                 // Text for "No" button
         }
@@ -86,4 +86,5 @@ const CONFIG = {
 };
 
 // Don't modify anything below this line unless you know what you're doing
+
 window.VALENTINE_CONFIG = CONFIG; 
